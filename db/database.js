456eff5c8bc3fb0356b52b366fdd19b75312f864
ElK <62822011+evkonradi@users.noreply.github.com>
2020-08-06 16:11:07 -0500

    select query

---
+++ db/database.js
@@ -0,0 +1,11 @@
+/* const mysql = require('mysql2');
+
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+      return console.error(err.message);
+    }
+  
+    console.log('Connected to the election database.');
+});
+
+module.exports = db; */
